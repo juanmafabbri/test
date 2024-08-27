c799673b28374e135f89adb1223c665cfab13d30
Juan Manuel Fabbri <32085323+juanmafabbri@users.noreply.github.com>
2024-08-27 15:09:29 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,15 @@
 import org.apache.commons.text.StringSubstitutor;
 
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 public class MyApp {
 
-    //static Logger logger = Logger.getLogger(MyApp.class.getName());
+    static Logger logger = Logger.getLogger(MyApp.class.getName());
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        //logger.info("Hello, World!");
+        logger.info("Hello, World!");
 
         System.out.println("more text");
         final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
