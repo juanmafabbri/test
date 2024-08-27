5e13d74767d025d13bea7b2807ed54e24095dd8b
Juan Manuel Fabbri <32085323+juanmafabbri@users.noreply.github.com>
2024-08-27 16:18:01 +0200

    Logger

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,18 +1,19 @@
 //import org.apache.commons.text.StringSubstitutor;
 
-import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class MyApp {
 
-    static Logger logger = Logger.getLogger(MyApp.class.getName());
+    static Logger logger = LogManager.getLogger(MyApp.class);
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        logger.info("Hello, World!");
+        logger.info("Hello, World! {}", 10);
 
         System.out.println("more text");
         //final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
         //String out = interpolator.replace("${script:javascript:java.lang.Runtime.getRuntime().exec('touch /tmp/foo')}");
     }
-}
+} 
